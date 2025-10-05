310ae82cda43496d4305334c37f6e510fb11b9b1
shaxruxpirimbetov <shaxruxpirimbetov@gmail.com>
2025-10-05 13:18:46 +0500

    update

--- www/js/config.js
+++ www/js/config.js
@@ -1,11 +1,26 @@
 // const baseUrl = "http://127.0.0.1:8000"
 const baseUrl = "https://shaxcoder.pythonanywhere.com"
-const cur_version = 1
+const cur_version = 0
 
 var accessToken = localStorage.getItem("accessToken")
 fetch(baseUrl+"/appmanage/", {
     method: "GET",
-    headers: {"Content-Type": "application/json", "authorization": `Bearer ${accessToken}`}
+    headers: {"Content-Type": "application/json"}
 })
 .then(res => res.json())
-.then(data => {console.log(data)})
+.then(data => {
+    console.log(data)
+    if (data.message.length <= 0) {return;}
+    var latest = data.message[data.message.length - 1]
+    
+    if (cur_version < latest.id) {
+        document.body.innerHTML += `
+    <div id="newUpdateBox">
+        <h3>New Update</h3>
+        <p>${latest.description}</p>
+        <div class="update-button" id="newUpdateButton">Update now</div>
+    </div>
+        `
+        document.getElementById("newUpdateButton").onclick = () => {window.open(latest.update_url, "_blank")}
+    }
+})
